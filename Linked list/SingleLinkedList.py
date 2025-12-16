3f0e0a771e8c912ef4c654a15a056d4e4913fbc8
SriPrazna-Lakkoju <sripraznalakkoju@gmail.com>
2025-12-16 19:02:18 +0530

    SingleLinkedList.py

--- Linked list/SingleLinkedList.py
+++ Linked list/SingleLinkedList.py
@@ -1,1 +1,20 @@
+# definition of a node
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.next = none
 
+#singly linked list:
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+head=node(10)
+head.next=node(20)
+head.next.next=node(30)
+head.next.next.next=node(40)
+
+temp = head
+while temp is not None :
+    print(temp.data,end=" ")
+    temp=temp.next
