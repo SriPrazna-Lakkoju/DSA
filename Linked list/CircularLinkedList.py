c2e379ff155c08eca3a4ec4ef2b193bd9e9f5b02
SriPrazna-Lakkoju <sripraznalakkoju@gmail.com>
2025-12-16 21:02:43 +0530

    CircularLinkedList.py

--- Linked list/CircularLinkedList.py
+++ Linked list/CircularLinkedList.py
@@ -1,1 +1,21 @@
+#circular linked list
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+head=node(10)
+head.next=node(20)
+head.next.next=node(30)
+head.next.next.next=node(40)
+head.next.next.next.next=head
 
+
+temp = head
+while True :
+    print(temp.data,end=" ")
+    temp=temp.next
+    if temp!=head:
+        print("->",end=" ")
+    else:
+        break
+print()
