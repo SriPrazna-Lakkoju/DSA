744db7403ff5a628af0aa05cf66007a80275b764
SriPrazna-Lakkoju <sripraznalakkoju@gmail.com>
2025-12-16 20:40:56 +0530

    DoublyLinkedList.py

--- Linked list/doublyLinkedList.py
+++ Linked list/doublyLinkedList.py
@@ -1,1 +1,26 @@
-
+#doubly linked list node definition:
+class node:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
+#doubly linked list
+class  Node:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
+if __name__ == "__main__":
+    head=Node(10)
+    head.next=Node(20)
+    head.next.prev=head
+    head.next.next=Node(30)
+    head.next.next.prev=head.next
+    head.next.next.next=Node(40)
+    head.next.next.next.prev=head.next.next
+    temp=head
+    while temp is not None:
+        print(temp.data,end=" ")
+        if temp.next is not None:
+            print("<->",end=" ")
+        temp=temp.next
